2c53377f50532656bdf904699147ac2718d76fce
rojinraj641 <rojinraj96@gmail.com>
2025-07-23 21:32:10 +0530

    Route changed from '/home' to '/'

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,12 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import './App.css'
 import Home from './Page/Home.jsx';
-import AboutMe from "./Components/AboutMe.jsx";
 
 function App() {
   return (
    <Router>
     <Routes>
-      <Route path="/home" element={<Home/>}></Route>
-      <Route path="/aboutMe" element={<AboutMe/>}></Route>
+      <Route path="/" element={<Home/>}></Route>
     </Routes>
    </Router>
   )
